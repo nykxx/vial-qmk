da2c9ae94013dec7054718167613c3c574895da3
y.o <tow.spin@gmail.com>
2026-08-01 14:46:21 +0900

    inertia調整

--- keyboards/omni_kbd/common/trackball_omni.h
+++ keyboards/omni_kbd/common/trackball_omni.h
@@ -5,20 +5,43 @@
 #include "../drivers/pmw33xx_common.h"
 
 #ifndef OMNI_SCROLL_INERTIA_DECAY
-#    define OMNI_SCROLL_INERTIA_DECAY 0.88f
+#    define OMNI_SCROLL_INERTIA_DECAY 0.95f
 #endif
 #ifndef OMNI_SCROLL_INERTIA_STOP
-#    define OMNI_SCROLL_INERTIA_STOP 0.08f
+#    define OMNI_SCROLL_INERTIA_STOP 0.01f
+#endif
+#ifndef OMNI_SCROLL_INERTIA_GAIN
+#    define OMNI_SCROLL_INERTIA_GAIN 1.5f
 #endif
 #ifndef OMNI_SCROLL_INERTIA_MAX
-#    define OMNI_SCROLL_INERTIA_MAX 60.0f
+#    define OMNI_SCROLL_INERTIA_MAX 80.0f
 #endif
+#ifndef OMNI_SCROLL_INERTIA_MIN_INPUT
+#    define OMNI_SCROLL_INERTIA_MIN_INPUT 0.5f
+#endif
+#ifndef OMNI_SCROLL_INERTIA_START_INPUT
+#    define OMNI_SCROLL_INERTIA_START_INPUT 4.0f
+#endif
+#ifndef OMNI_SCROLL_INERTIA_INTERVAL_MS
+#    define OMNI_SCROLL_INERTIA_INTERVAL_MS 8
+#endif
+#ifndef OMNI_SCROLL_INERTIA_AXIS_SWITCH_RATIO
+#    define OMNI_SCROLL_INERTIA_AXIS_SWITCH_RATIO 1.5f
+#endif
+
+typedef enum {
+    TRACKBALL_SCROLL_AXIS_NONE = 0,
+    TRACKBALL_SCROLL_AXIS_H,
+    TRACKBALL_SCROLL_AXIS_V,
+} trackball_scroll_axis_t;
 
 typedef struct {
     float accumulated_h;
     float accumulated_v;
     float inertia_h;
     float inertia_v;
+    uint16_t last_inertia_timer;
+    trackball_scroll_axis_t active_axis;
 } trackball_scroll_state_t;
 
 void process_cursor_report(report_mouse_t *mouse_report, pmw33xx_report_t report, float speed_adjust, uint8_t slope_factor, int rx, int ry, uint8_t cpi_scale);
